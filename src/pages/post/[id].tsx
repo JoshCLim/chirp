4592ff50459ca80226979ace351913ff81c8cc44
Josh <joshconstantinelim@gmail.com>
2023-05-15 15:39:51 +1000

    post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,28 +1,52 @@
-import { useUser } from "@clerk/nextjs";
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "~/utils/api";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
+import { PostView } from "~/components/postView";
 
-const ProfilePage: NextPage = () => {
-  const { isLoaded: userLoaded, isSignedIn } = useUser();
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
-  api.posts.getAll.useQuery();
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
 
-  if (!userLoaded) return <div></div>;
+  if (!data) return <div>404</div>;
 
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">Post Page</main>
+      <main className="flex h-screen justify-center">
+        <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
+          <PostView {...data} />
+        </div>
+      </main>
     </>
   );
 };
 
-export default ProfilePage;
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("no id");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
+
+export default SinglePostPage;
